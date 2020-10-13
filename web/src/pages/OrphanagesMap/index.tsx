2cde2ab45a353c900580338e4e785ba61ace5c8c
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-10-12 21:48:14 -0300

    create component and styeled him

--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+import { FiPlus } from "react-icons/fi";
+
+import mapMarker from "../../images/map-marker.svg";
+
+import '../../styles/pages/orphanages-map.css'
 
 function OrphanegesMap() {
   return (
     <>
-        <h1>OrphanegesMap</h1>
+      <div id="page-map">
+        <aside>
+          <header>
+            <img src={mapMarker} alt="mapMarker" />
+
+            <h2>Escolha um orfanato no mapa</h2>
+            <p>Muitas crianças estão esperando a sua visita.</p>
+          </header>
+
+          <footer>
+            <strong>São Paulo</strong>
+            <span>Itaquaquecetuba</span>
+          </footer>
+        </aside>
+        <div></div>
+        <Link to="" className="create-orphanage">
+          <FiPlus size={32} color="#FFF" />
+        </Link>
+      </div>
     </>
   );
 }
-export default OrphanegesMap
+export default OrphanegesMap;
