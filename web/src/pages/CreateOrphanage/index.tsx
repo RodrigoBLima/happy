3e279a5d676096109c4d11a83a9a646da03a13ae
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-10-14 12:12:25 -0300

    create sidebar component

--- web/src/pages/CreateOrphanage/index.tsx
+++ web/src/pages/CreateOrphanage/index.tsx
@@ -1,52 +1,49 @@
 import React from "react";
-import { Map, Marker, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
+import { Map, Marker, TileLayer } from "react-leaflet";
+import L from "leaflet";
 import { useHistory } from "react-router-dom";
 
-import { FiArrowLeft, FiPlus } from "react-icons/fi";
+import { FiPlus } from "react-icons/fi";
 
-import mapMarkerImg from '../../images/map-marker.svg';
+import mapMarkerImg from "../../images/map-marker.svg";
 
-import '../../styles/pages/create-orphanage.css';
+import "../../styles/pages/create-orphanage.css";
+import Sidebar from "../../components/Sidebar";
 
 const happyMapIcon = L.icon({
   iconUrl: mapMarkerImg,
 
   iconSize: [58, 68],
   iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-})
+  popupAnchor: [0, -60],
+});
 
 export default function CreateOrphanage() {
   const { goBack } = useHistory();
 
   return (
     <div id="page-create-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      <Sidebar />
 
       <main>
         <form className="create-orphanage-form">
           <fieldset>
             <legend>Dados</legend>
 
-            <Map 
-              center={[-27.2092052,-49.6401092]} 
-              style={{ width: '100%', height: 280 }}
+            <Map
+              center={[-27.2092052, -49.6401092]}
+              style={{ width: "100%", height: 280 }}
               zoom={15}
             >
-              <TileLayer 
+              <TileLayer
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
-              <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052,-49.6401092]} />
+              <Marker
+                interactive={false}
+                icon={happyMapIcon}
+                position={[-27.2092052, -49.6401092]}
+              />
             </Map>
 
             <div className="input-block">
@@ -55,16 +52,16 @@ export default function CreateOrphanage() {
             </div>
 
             <div className="input-block">
-              <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
+              <label htmlFor="about">
+                Sobre <span>Máximo de 300 caracteres</span>
+              </label>
               <textarea id="name" maxLength={300} />
             </div>
 
             <div className="input-block">
               <label htmlFor="images">Fotos</label>
 
-              <div className="uploaded-image">
-
-              </div>
+              <div className="uploaded-image"></div>
 
               <button className="new-image">
                 <FiPlus size={24} color="#15b6d6" />
@@ -89,7 +86,9 @@ export default function CreateOrphanage() {
               <label htmlFor="open_on_weekends">Atende fim de semana</label>
 
               <div className="button-select">
-                <button type="button" className="active">Sim</button>
+                <button type="button" className="active">
+                  Sim
+                </button>
                 <button type="button">Não</button>
               </div>
             </div>
