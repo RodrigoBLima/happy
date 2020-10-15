9de0c0561ebb5b5a79e21af29af0ee0c93d06dfc
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-10-15 08:51:20 -0300

    create routes, components, add types

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native'
+
+const {Navigator,Screen} = createStackNavigator()
+
+import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false }}>
+                <Screen name="OrphanagesMap" component={OrphanagesMap} />
+                <Screen name="OrphanegesDetails" component={OrphanageDetails} />
+            </Navigator>
+        </NavigationContainer>
+    )
+}
