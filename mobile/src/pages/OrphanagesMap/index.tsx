d1517aaf66ccf2907d30c3b3dcceb97b5d994aaa
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-10-15 09:35:25 -0300

    add map, add images

--- mobile/src/pages/OrphanagesMap/index.tsx
+++ mobile/src/pages/OrphanagesMap/index.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 const OrphanagesMap: React.FC = () => {
   return (
-    <View style={ styles.container }>
-      <Text style={ styles.title }>OrphanagesMap</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.mapStyle}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: -21.235374444444446,
+          longitude: -47.409243333333336,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#312e38',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#312e38",
   },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 22,
-    color: '#fff',
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
 
