59729d55653c6e1ff04c1cfe82bc8f7c30424a09
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-10-15 10:29:03 -0300

    add font, styled component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import Routes from './src/routes';
+import React, { useState, useEffect } from "react";
+import { AppLoading } from "expo";
+import { useFonts } from "expo-font";
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from "@expo-google-fonts/nunito";
+
+import Routes from "./src/routes";
 
 export default function App() {
-  return (
-    <Routes />
-  );
+  let [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return <Routes />;
+  }
 }
