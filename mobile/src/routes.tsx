9e32490f2ab876fd2b78ca0f26a70701e277c3ee
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-10-16 11:06:59 -0300

    routes

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -7,13 +7,29 @@ const { Navigator, Screen } = createStackNavigator();
 
 import OrphanagesMap from "./pages/OrphanagesMap";
 import OrphanageDetails from "./pages/OrphanageDetails";
+import SelectMapPosition from "./pages/CreateOrphanage/SelectMapPosition";
+import OrphanageData from "./pages/CreateOrphanage/OrphanageData";
 
 export default function Routes() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
-        <Screen name="OrphanagesMap" component={OrphanagesMap} />
-        <Screen name="OrphanegesDetails" component={OrphanageDetails} />
+        <Screen 
+          name="OrphanagesMap" 
+          component={OrphanagesMap} 
+        />
+        <Screen 
+          name="OrphanegesDetails" 
+          component={OrphanageDetails} 
+        />
+        <Screen 
+          name="OrphanageData" 
+          component={OrphanageData} 
+        />
+        <Screen 
+          name="SelectMapPosition" 
+          component={SelectMapPosition} 
+        />
       </Navigator>
     </NavigationContainer>
   );
