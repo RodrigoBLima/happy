4ddf720b65a616469667b6dc403b4e8c11227c8c
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-10-16 12:53:28 -0300

    orphanage details

--- server/src/views/ImagesView.ts
+++ server/src/views/ImagesView.ts
@@ -4,7 +4,7 @@ export default {
   render(image: Image) {
     return {
       id: image.id,
-      url: `http://localhost:3333/uploads/${image.path}`,
+      url: `http://10.0.0.110:3333/uploads/${image.path}`,
     };
   },
   renderMany(images: Image[]) {
